c01217a706dd7333e176f9a18ab7fb2924c7b87a
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2024-12-04 22:06:12 +0100

    Update deployment to prd

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import Card from "@/components/Card";
-import PieChart from "@/components/PieChart";
 import List from "@/components/List";
 
 const Home: React.FC = () => {
@@ -17,11 +16,6 @@ const Home: React.FC = () => {
             { label: "Children", value: "2000" },
           ]}
         />
-      </Card>
-      <Card>
-        <div className="flex items-center justify-center">
-          <PieChart />
-        </div>
       </Card>
     </>
   );

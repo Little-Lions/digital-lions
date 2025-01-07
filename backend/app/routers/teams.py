d34eceabd587af894fe01ac686838b610a6a2331
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2025-01-07 16:01:45 +0100

    Add team

--- backend/app/routers/teams.py
+++ backend/app/routers/teams.py
@@ -2,7 +2,8 @@ import logging
 from typing import Annotated
 
 from core import exceptions
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi import status as http_status
 from models import team as models
 from models.generic import Message, RecordCreated
 from routers._responses import with_default_responses
@@ -16,15 +17,15 @@ router = APIRouter(prefix="/teams")
 @router.post(
     "",
     response_model=RecordCreated,
-    status_code=status.HTTP_201_CREATED,
+    status_code=http_status.HTTP_201_CREATED,
     summary="Create a new team",
     responses=with_default_responses(
         {
-            status.HTTP_404_NOT_FOUND: {
+            http_status.HTTP_404_NOT_FOUND: {
                 "model": Message,
                 "description": "Community not found",
             },
-            status.HTTP_409_CONFLICT: {
+            http_status.HTTP_409_CONFLICT: {
                 "model": Message,
                 "description": "Conflict: team name already exists",
             },
@@ -48,17 +49,21 @@ async def post_team(
     try:
         return team_service.create(team)
     except exceptions.CommunityNotFoundError as exc:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(exc))
+        raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND, detail=str(exc))
     except exceptions.TeamAlreadyExistsError as exc:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(exc))
+        raise HTTPException(status_code=http_status.HTTP_409_CONFLICT, detail=str(exc))
     except exceptions.InsufficientPermissionsError as exc:
+<<<<<<< HEAD
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(exc))
+=======
+        raise HTTPexception(status_code=http_status.HTTP_403_FORBIDDEN, detail=str(exc))
+>>>>>>> 6fd1f88 (Fix name shadowing)
 
 
 @router.get(
     "",
     response_model=list[models.TeamGetOut],
-    status_code=status.HTTP_200_OK,
+    status_code=http_status.HTTP_200_OK,
     summary="Get teams",
     responses=with_default_responses(),
 )
@@ -77,17 +82,17 @@ async def get_teams(
     try:
         return team_service.get_all(community_id=community_id, status=status)
     except exceptions.InsufficientPermissionsError as exc:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(exc))
+        raise HTTPException(status_code=http_status.HTTP_403_FORBIDDEN, detail=str(exc))
 
 
 @router.get(
     "/{team_id}",
     response_model=models.TeamGetByIdOut,
-    status_code=status.HTTP_200_OK,
+    status_code=http_status.HTTP_200_OK,
     summary="Get team by id",
     responses=with_default_responses(
         {
-            status.HTTP_404_NOT_FOUND: {
+            http_status.HTTP_404_NOT_FOUND: {
                 "model": Message,
                 "description": "Team not found",
             },
@@ -109,25 +114,25 @@ async def get_team(
         return team_service.get(object_id=team_id)
     except exceptions.TeamNotFoundError as exc:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
+            status_code=http_status.HTTP_404_NOT_FOUND,
             detail=str(exc),
         )
     except exceptions.InsufficientPermissionsError as exc:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(exc))
+        raise HTTPException(status_code=http_status.HTTP_403_FORBIDDEN, detail=str(exc))
 
 
 @router.delete(
     "/{team_id}",
-    status_code=status.HTTP_200_OK,
+    status_code=http_status.HTTP_200_OK,
     summary="Delete a team",
     response_model=Message,
     responses=with_default_responses(
         {
-            status.HTTP_404_NOT_FOUND: {
+            http_status.HTTP_404_NOT_FOUND: {
                 "model": Message,
                 "description": "Team not found",
             },
-            status.HTTP_409_CONFLICT: {
+            http_status.HTTP_409_CONFLICT: {
                 "model": Message,
                 "description": "Conflict: team has children and cascade is False",
             },
@@ -153,10 +158,10 @@ async def delete_team(
         return team_service.delete(object_id=team_id, cascade=cascade)
     except exceptions.TeamHasChildrenError as exc:
         raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
+            status_code=http_status.HTTP_409_CONFLICT,
             detail=str(exc),
         )
     except exceptions.TeamNotFoundError as exc:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(exc))
+        raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND, detail=str(exc))
     except exceptions.InsufficientPermissionsError as exc:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(exc))
+        raise HTTPException(status_code=http_status.HTTP_403_FORBIDDEN, detail=str(exc))
